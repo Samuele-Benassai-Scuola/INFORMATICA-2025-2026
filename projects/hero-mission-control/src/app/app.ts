ab3ac2f97eb6c770bb8cf271a92f13fe7a394ae3
tucanofasano <samuelebenassai.pptx@gmail.com>
2026-04-14 09:26:59 +0200

    Added hero-mission-control

---
+++ projects/hero-mission-control/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroList } from "./components/hero-list/hero-list";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeroList],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('hero-mission-control');
+}
---
+++ projects/hero-mission-control/src/app/components/hero-list/hero-list.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../models/hero';
+import { CommonModule } from '@angular/common';
+import { HeroCard } from '../hero-card/hero-card';
+
+@Component({
+  selector: 'app-hero-list',
+  imports: [CommonModule, HeroCard],
+  templateUrl: './hero-list.html',
+  styleUrl: './hero-list.css',
+})
+export class HeroList {
+
+  list: Hero[] = [
+    { id: 1, name: "A", power: "a", completed: false },
+    { id: 2, name: "B", power: "b", completed: false },
+    { id: 3, name: "C", power: "c", completed: false },
+    { id: 4, name: "D", power: "d", completed: false },
+    { id: 5, name: "E", power: "e", completed: false }
+  ]
+
+  totalCompleted: number = this.countTotalCompleted()
+
+  handleCompleted(hero: Hero) {
+    hero.completed = true
+    this.totalCompleted = this.countTotalCompleted()
+  }
+
+  countTotalCompleted(): number {
+    return this.totalCompleted = this.list.filter(h => h.completed).length
+  }
+}
